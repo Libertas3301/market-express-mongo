cbdecbf443bbf6673bd6f472a04df0220ddc9f92
Libertas3301 <57844736+Libertas3301@users.noreply.github.com>
2020-01-10 04:53:54 -1000

    first commit

--- controllers/home.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-const home = (req, res) => {
-    return res.sendFile(path.join(`${__dirname}/../views/shop/index.hbs`));
-};
-
-module.exports = {
-    getHome: home
-};
