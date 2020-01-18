bffe7802b1f8b8fb7496c19df83c61c2d15b3bb1
Libertas3301 <57844736+Libertas3301@users.noreply.github.com>
2020-01-18 02:09:19 -1000

    stripe payment

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const multer = require('multer');
 const url = require('url');
 
-const Product = require('../models/product')
-const Cart = require('../models/cart')
+var Cart = require('../models/cart');
+var Product = require('../models/product');
+var Order = require('../models/order');
 const User = require('../models/user');
 
 const mongoose = require('mongoose');
@@ -61,6 +62,71 @@ router.get('/shopping-cart', (req, res, next) => {
   let cart = new Cart(req.session.cart);
   res.render('shop/shopping-cart.hbs', { products: cart.generateArray(), totalPrice: cart.totalPrice })
 });
+
+router.get('/reduce/:id', function (req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {});
+  cart.reduceByOne(productId);
+  req.session.cart = cart;
+  res.redirect('/shopping-cart');
+});
+
+router.get('/remove/:id', function (req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+  cart.removeItem(productId);
+  req.session.cart = cart;
+  res.redirect('/shopping-cart');
+});
+
+router.get('/checkout', isLoggedIn, function (req, res, next) {
+  if (!req.session.cart) {
+    return res.redirect('/shopping-cart');
+  }
+  var cart = new Cart(req.session.cart);
+  var errMsg = req.flash('error')[0];
+  res.render('shop/checkout.hbs', { total: cart.totalPrice, errMsg: errMsg, noError: !errMsg });
+});
+
+router.post('/checkout', function (req, res, next) {
+  if (!req.session.cart) {
+    return res.redirect('/shopping-cart');
+  }
+  var cart = new Cart(req.session.cart);
+
+  var stripe = require("stripe")(
+    "sk_test_RDjVRjleG1h4ZQBLfKSp2VcV00ce92e5LA"
+  );
+
+  stripe.charges.create({
+    amount: cart.totalPrice * 100,
+    currency: "usd",
+    source: req.body.stripeToken, // obtained with Stripe.js
+    description: "Test Charge"
+  }, function (err, charge) {
+    if (err) {
+      req.flash('error', err.message);
+      return res.redirect('/checkout');
+    }
+    var order = new Order({
+      user: req.user,
+      cart: cart,
+      address: req.body.address,
+      name: req.body.name,
+      paymentId: charge.id
+    });
+    order.save(function (err, result) {
+      req.flash('success', 'Successfully bought product!');
+      req.session.cart = null;
+      res.redirect('/');
+    });
+  });
+});
+
+// router.post('/checkout', (req, res, next) => {
+//   res.send('ebati');
+// });
 
 // router.post('/deletePost', (req, res) => {
 //   Product.findOneAndRemove({ '_id': req.body.id }, (err, offer) => {
